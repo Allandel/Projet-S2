6331169a8fe46de43b5027f50201eb4696b0c665
louis <louis@debian00.org>
2016-03-15 16:30:41 +0100

    Voisin et accessible

--- Projet S2/src/Case.java
+++ Projet S2/src/Case.java
@@ -5,5 +5,24 @@ public class Case {
 	
 	}
 	
-
+	int nbrVoisin(int [][]plateau,int x, int y){
+		int nbr=0;
+		
+		if(plateau[x+1][y]!=0)
+			nbr++;
+		if(plateau[x-1][y]!=0)
+			nbr++;
+		if(plateau[x][y+1]!=0)
+			nbr++;
+		if(plateau[x][y-1]!=0)
+			nbr++;
+		
+		return nbr;
+	}
+	
+	boolean estAccessible(int [][]plateau,int x, int y){
+		if(this.nbrVoisin(plateau, x, y)<4)
+			return true;
+		return false;
+	}
 }
