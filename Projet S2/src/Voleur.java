9614e4c6bdc7891bd8eb04aaacea784bb24d07c7
tartarev <tartarev@OffensiveBias>
2016-04-20 10:57:29 +0200

    Correction du vol

--- Projet S2/src/Voleur.java
+++ Projet S2/src/Voleur.java
@@ -29,17 +29,23 @@ public class Voleur extends Personnage {
 	public void volerObjet(Personnage p, int x, int y, Case[][] tableauIle){
 		Random random=new Random();
 		if (this.equipe1!=p.equipe1){
-			if(!p.getInventaire().isEmpty() && random.nextInt(4)==2 ){
+			if(p.getInventaire().isEmpty() && random.nextInt(4)==2 ){
+				Object[] options = { "OK" };
+				JOptionPane.showOptionDialog(null, "Votre cible ne possede rien dans son inventaire...", "VOL IMPOSSIBLE",
+						JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
+						null, options, options[0]);
+	
+			}else if(!p.getInventaire().isEmpty() && random.nextInt(4)==2 ){
 				int objetVole=random.nextInt(p.getInventaire().size());
 				getInventaire().add(p.getInventaire().get(objetVole));
 				p.getInventaire().remove(objetVole);
 				Object[] options = { "OK" };
-				JOptionPane.showOptionDialog(null, "Vous avez vol� : "+getInventaire().get(getInventaire().size()-1)+" avec un franc succ�s ! Vous �tes un fin voleur !", "VOL REUSSI",
+				JOptionPane.showOptionDialog(null, "Vous avez voler : "+getInventaire().get(getInventaire().size()-1)+" avec un franc succ�s ! Vous �tes un fin voleur !", "VOL REUSSI",
 						JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
 						null, options, options[0]);
 			}else{
 				Object[] options = { "OK" };
-				JOptionPane.showOptionDialog(null, "Vous avez �chou� votre vol... Peut etre aurez vous plus de chance la prochaine fois", "VOL ECHEC",
+				JOptionPane.showOptionDialog(null, "Vous avez echoue votre vol... Peut etre aurez vous plus de chance la prochaine fois", "VOL ECHEC",
 						JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
 						null, options, options[0]);
 			}
