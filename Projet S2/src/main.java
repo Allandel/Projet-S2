7e588d41d79d163e320233649d01ec6e14be7587
Allan <Allan@Allan-PC>
2016-04-24 23:38:21 +0200

    tests

--- Projet S2/src/main.java
+++ Projet S2/src/main.java
@@ -6,11 +6,18 @@
 public class main {
 
 	public static void main(String[] args) {
-		Test test=new Test();
-		test.testPerso(null);
-		/*
+//		Test test=new Test();
+//		test.testPerso(null);
+		
 		Launcher menu = new Launcher();		
 		while(Launcher.getetat()){
+			if(Launcher.getid() == 1){ /* test 1 */
+				Test test=new Test();
+				test.testPerso(null);
+			}
+			if(Launcher.getid() == 2) { /* test2 */
+				
+			}
 			System.out.flush();
 		}
 		GestionDuJeu gestion=new GestionDuJeu();
@@ -20,7 +27,7 @@ public class main {
 		}while(!fin[0]);
 
 		Affichage victoire=new Affichage(fin);
-		 */
+		 
 		System.exit(0);
 
 	}
